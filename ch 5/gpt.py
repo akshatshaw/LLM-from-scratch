531bba9fd21148d37fcee1824db706a384c1f944
akshatshaw <akshaw.ak4@gmail.com>
2024-12-28 02:17:05 +0530

    ch6 and minor changes

--- ch 5/gpt.py
+++ ch 5/gpt.py
@@ -198,3 +198,33 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
         idx = torch.cat((idx, idx_next), dim=1)
     return idx
 
+# The final Generate function
+# Note that if temperature is set to 0.0, the function will behave like argmax and Softmax will not be applied.
+
+def generate(model, idx, max_new_tokens, context_size,
+    temperature=0.0, top_k=None, eos_id=None):
+    for _ in range(max_new_tokens): # The for loop is the same as before: gets logits and only focuses on the last time step.
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+            logits = logits[:, -1, :]
+            if top_k is not None: # filters logits to only consider the top-k tokens
+                top_logits, _ = torch.topk(logits, top_k)
+                min_val = top_logits[:, -1]
+                logits = torch.where(
+                    logits < min_val,
+                    torch.tensor(float('-inf')).to(logits.device),
+                    logits
+                )
+            if temperature > 0.0: # applies temperature scaling
+                logits = logits / temperature
+                probs = torch.softmax(logits, dim=-1)
+                idx_next = torch.multinomial(probs, num_samples=1)
+            else: 
+                idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+            if idx_next == eos_id: # Stops generating early if end-of-sequence token is encountered
+                break
+            idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+
+
